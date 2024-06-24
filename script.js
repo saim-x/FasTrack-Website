9085fa2750849e2c44f0f8d0a253e2f9cc98599f
saim-x <x_aci@hotmail.com>
2024-06-25 00:26:52 +0500

    UI changes

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 // document.getElementById('downloadButton').addEventListener('click', function() {
 //     window.location.href = 'path_to_your_apk_file.apk';
 // });
+
+
+function changeButtonText() {
+    window.open('app.apk')
+    var button = document.getElementById('downloadButton');
+    button.innerText = 'Download horhi hai sabar rkhein';
+
+    setTimeout(function () {
+        button.innerText = 'Download APK (25M)';
+    }, 5000); 
+}
