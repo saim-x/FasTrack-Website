868c6c5fce2ad47cd0b5884c19e4611bf770c191
saim-x <x_aci@hotmail.com>
2024-06-25 14:31:03 +0500

    UI changes

--- script.js
+++ script.js
@@ -5,10 +5,16 @@
 
 function changeButtonText() {
     window.open('app.apk')
+    var buttonText = document.querySelector('#downloadButtonPet .button__text');
+    buttonText.textContent = 'Downloading...';
     // var button = document.getElementById('downloadButton');
     // button.innerText = 'Download horhi hai sabar rkhein';
 
-    // setTimeout(function () {
-    //     button.innerText = 'Download APK (25M)';
-    // }, 5000); 
+    setTimeout(function () {
+        buttonText.textContent = 'Download';
+    }, 5000); 
 }
+document.getElementById('downloadButtonPet').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default action of the link
+    changeButtonText();     // Call the function
+});
